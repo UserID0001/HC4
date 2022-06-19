9d3278d136a4d59cce9167968b6d3e424f13eb53
UserID0001 <103254369+UserID0001@users.noreply.github.com>
2022-06-20 05:00:37 +0800

    Update date2.js

--- js/date2.js
+++ js/date2.js
@@ -1,7 +1,7 @@
 
   function showDate() {
     var date = new Date();
-    var d = date.getDate();
+    var d = date.getDate() - 1;
     var mon = 1 + date.getMonth();
     var y = date.getFullYear();
 
@@ -12,7 +12,7 @@
 
    
     var date = mon + "月" + d + "日";
-     document.getElementById("lowerBody1c").innerText = date + "\xa0\xa0" + "9:38";
+     document.getElementById("lowerBody1c").innerText = date + "\xa0\xa0" + "8:36";
      //document.getElementById("MyClockDisplay").textContent = time;
      //setTimeout(showTime, 1000);
     }

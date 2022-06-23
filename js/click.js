3bb8b237e43e12570babf0f699291b32f4975f15
UserID0001 <103254369+UserID0001@users.noreply.github.com>
2022-06-23 22:23:29 +0800

    Update click.js

--- js/click.js
+++ js/click.js
@@ -1,4 +1,12 @@
-function myFunction () {
+
+    var count = 0;
+    function myFunction () {
+    count = count + 1;
+    if (count%2 == 1){
     document.getElementById("xxx").innerHTML="张魁";
     document.getElementById("yyy").innerHTML="410821198410060013";
+}else{
+    document.getElementById("xxx").innerHTML="张*";
+    document.getElementById("yyy").innerHTML="410***********0013";
+}
 }

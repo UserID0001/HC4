ff0062de83c596cd7f7f9a6cd0c63c5cd4a527e2
UserID0001 <103254369+UserID0001@users.noreply.github.com>
2022-07-24 00:14:58 +0800

    Update time.js

--- js/time.js
+++ js/time.js
@@ -5,22 +5,22 @@
     var m = date.getMinutes();
     var s = date.getSeconds();
 
-    if(h ==0){
-        h =12
+    if(h == 0){
+        h = 00
     }
 
     if (h < 10){
-        h = "0" +h;
+        h = "0" + h;
     }
 
-    if(m<10){
+    if(m < 10){
         m = "0" + m;
     }
 
-    if (s<10) {
-        s= "0" +s;
+    if (s < 10) {
+        s = "0" + s;
     }
-    var time = h + ":" + m + ":" +s;
+    var time = h + ":" + m + ":" + s;
      document.getElementById("MyClockDisplay").innerText = time;
      document.getElementById("MyClockDisplay").textContent = time;
      setTimeout(showTime, 1000);
